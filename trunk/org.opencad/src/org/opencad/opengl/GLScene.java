0d2f98e250d6bcade20a3b9b87dbba0e2dd81334
george-a <george-a@45268663-b410-0410-bcd7-8697c71b8d56>
2006-05-30 12:57:41 +0000

    * Added extra files

---
+++ trunk/org.opencad/src/org/opencad/opengl/GLScene.java
@@ -0,0 +1,178 @@
+/***************************************************************************************************
+ * Copyright (c) 2005 Bo Majewski All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/org/documents/epl-v10.html Contributors:
+ * Bo Majewski - initial API and implementation
+ **************************************************************************************************/
+package org.opencad.scene;
+
+import org.eclipse.opengl.GL;
+import org.eclipse.opengl.GLU;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.opengl.GLCanvas;
+import org.eclipse.swt.opengl.GLData;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * Creates an OpenGL scene. In order to draw on it, overrider the <code>drawScene</code> method.
+ * 
+ * @author Bo Majewski
+ */
+public class GLScene {
+	public static final int PERSPECTIVE = 0;
+	public static final int ORTHOGRAPHIC = 1;
+	private GLCanvas canvas;
+	private int projectionStyle;
+	private SceneGrip grip;
+
+	/**
+	 * Creates a new scene owned by the specified parent component.
+	 * 
+	 * @param parent
+	 *          The parent of this scene.
+	 */
+	public GLScene(Composite parent, int projectionStyle) {
+		GLData data = new GLData();
+		this.projectionStyle = projectionStyle;
+		data.depthSize = 1;
+		data.doubleBuffer = true;
+		this.canvas = new GLCanvas(parent, SWT.NO_BACKGROUND, data);
+		this.canvas.addControlListener(new ControlAdapter() {
+			public void controlResized(ControlEvent e) {
+				resizeScene();
+			}
+		});
+		this.canvas.addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e) {
+				dispose();
+			}
+		});
+		this.init();
+		Rectangle clientArea = parent.getClientArea();
+		this.canvas.setSize(clientArea.width, clientArea.height);
+		this.grip = new SceneGrip(this);
+		this.grip.setOffsets(-3.25f, 3.25f, -30.5f);
+		this.grip.setRotation(45.0f, -30.0f);
+		this.getCanvas().addMouseListener(this.grip);
+		this.getCanvas().addMouseMoveListener(this.grip);
+		this.getCanvas().addListener(SWT.MouseWheel, this.grip);
+		this.getCanvas().addKeyListener(this.grip);
+	}
+	/**
+	 * Initializes this scene, by calling the <code>initGLContext</code> and <code>initGL</code>
+	 * method.
+	 */
+	protected void init() {
+		this.initGLContext();
+		this.initGL();
+	}
+	/**
+	 * Disposes of the GL context. This method is called when the canvas is disposed.
+	 */
+	protected void dispose() {
+	}
+	/**
+	 * Returns whether or not this scene is disposed.
+	 * 
+	 * @return Whether or not the scene is disposed.
+	 */
+	public boolean isDisposed() {
+		return this.canvas.isDisposed();
+	}
+	/**
+	 * Causes the receiver to have the <em>keyboard focus</em>,
+	 * 
+	 * @return Whether or not the control got focus.
+	 */
+	public boolean setFocus() {
+		return this.canvas.setFocus();
+	}
+	/**
+	 * Provides direct access to this GL Canvas.
+	 * 
+	 * @return The GL canvas of this scene.
+	 */
+	protected GLCanvas getGLContext() {
+		return this.canvas;
+	}
+	/**
+	 * Returns the drawable used by the GLContext to render GL scenes.
+	 * 
+	 * @return The canvas used by the GL context.
+	 */
+	protected Canvas getCanvas() {
+		return this.canvas;
+	}
+	public Display getDisplay() {
+		return this.canvas.getDisplay();
+	}
+	// -------------------------------------------------------------------------
+	// Public API
+	// -------------------------------------------------------------------------
+	/**
+	 * Renders the next scene.
+	 */
+	public void render() {
+		if (!this.canvas.isCurrent()) {
+			this.canvas.setCurrent();
+		}
+		this.drawScene();
+		this.canvas.swapBuffers();
+	}
+	// -------------------------------------------------------------------------
+	// OpenGL
+	// -------------------------------------------------------------------------
+	protected void initGLContext() {
+		this.canvas.setCurrent(); // Activate the rendering context
+	}
+	/**
+	 * Initializes OpenGL by creating a context, setting background, shading model, and such.
+	 */
+	protected void initGL() {
+		GL.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		GL.glClearDepth(1.0f);
+		GL.glDepthFunc(GL.GL_LESS);
+		GL.glEnable(GL.GL_DEPTH_TEST);
+		GL.glShadeModel(GL.GL_SMOOTH);
+		GL.glHint(GL.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
+	}
+	/**
+	 * Corrects the size of the GL scene.
+	 */
+	protected void resizeScene() {
+		Rectangle rect = this.canvas.getClientArea();
+		GL.glViewport(0, 0, rect.width, rect.height);
+		GL.glMatrixMode(GL.GL_PROJECTION);
+		GL.glLoadIdentity();
+		setupProjection(rect);
+		GL.glMatrixMode(GL.GL_MODELVIEW);
+		GL.glLoadIdentity();
+	}
+	/**
+	 * @param rect
+	 */
+	protected void setupProjection(Rectangle rect) {
+		if (projectionStyle == PERSPECTIVE) {
+			GLU.gluPerspective(45.0f, (float) rect.width / (float) rect.height, 0.1f, 100.0f);
+		} else if (projectionStyle == ORTHOGRAPHIC) {
+			final float aspect = (float) rect.width / (float) rect.height;
+			float size = 10f;
+			GL.glOrtho(-size * aspect, size * aspect, -size, size, 1, 1000);
+		}
+	}
+	/**
+	 * Draws the GL scene. The default implementation clears the scene and resets the view.
+	 */
+	protected void drawScene() {
+		GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+		GL.glLoadIdentity();
+		this.grip.adjust();
+	}
+}

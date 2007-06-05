d2aef3863ee942625fbed2e1885f26c935746e05
opencad <opencad@45268663-b410-0410-bcd7-8697c71b8d56>
2007-06-05 14:36:48 +0000

    faster inner angle clipping

--- trunk/org.opencad.core/src/org/opencad/modelling/corners/Corner.java
+++ trunk/org.opencad.core/src/org/opencad/modelling/corners/Corner.java
@@ -1,8 +1,9 @@
 package org.opencad.modelling.corners;
 
 import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedMap;
 import java.util.TreeMap;
-
 import org.eclipse.opengl.GL;
 import org.opencad.modelling.Primitive;
 import org.opencad.modelling.PrimitiveTypeRegister;
@@ -13,207 +14,213 @@ import org.opencad.ui.editor.Hoverable;
 import org.opencad.ui.editor.Selectable;
 
 public class Corner extends Primitive implements Hoverable, Selectable {
+  private static final long serialVersionUID = -1332083715502519329L;
+  static {
+    PrimitiveTypeRegister.registerPrimitiveType(Corner.class);
+  }
 
-	private static final long serialVersionUID = -1332083715502519329L;
+  private Double x, y;
 
-	static {
-		PrimitiveTypeRegister.registerPrimitiveType(Corner.class);
-	}
+  private transient boolean hover;
 
-	private Double x, y;
+  private transient boolean selected;
 
-	private transient boolean hover;
+  public static double thickness = 0.05d;
 
-	private transient boolean selected;
+  private static double hoverSlack = 0.06d;
 
-	private static double thickness = 0.05d;
+  private HashSet<Wall> startOf = new HashSet<Wall>();
 
-	private static double hoverSlack = 0.06d;
+  private HashSet<Wall> endOf = new HashSet<Wall>();
 
-	private HashSet<Wall> startOf = new HashSet<Wall>();
+  public void addStart(Wall start) {
+    startOf.add(start);
+  }
 
-	private HashSet<Wall> endOf = new HashSet<Wall>();
+  public void removeStart(Wall start) {
+    startOf.remove(start);
+  }
 
-	public void addStart(Wall start) {
-		startOf.add(start);
-	}
+  public void addEnd(Wall start) {
+    endOf.add(start);
+  }
 
-	public void removeStart(Wall start) {
-		startOf.remove(start);
-	}
+  public void removeEnd(Wall start) {
+    endOf.remove(start);
+  }
 
-	public void addEnd(Wall start) {
-		endOf.add(start);
-	}
+  public final Double getX() {
+    return x;
+  }
 
-	public void removeEnd(Wall start) {
-		endOf.remove(start);
-	}
+  public final void setX(Double x) {
+    this.x = x;
+  }
 
-	public final Double getX() {
-		return x;
-	}
+  public final Double getY() {
+    return y;
+  }
 
-	public final void setX(Double x) {
-		this.x = x;
-	}
+  public final void setY(Double y) {
+    this.y = y;
+  }
 
-	public final Double getY() {
-		return y;
-	}
+  public Corner(Double x, Double y) {
+    this.x = x;
+    this.y = y;
+  }
 
-	public final void setY(Double y) {
-		this.y = y;
-	}
+  public String toString() {
+    return String.format("(corner %.2f:%.2f)", x, y);
+  }
 
-	public Corner(Double x, Double y) {
-		this.x = x;
-		this.y = y;
-	}
+  public boolean isHoverCoordinates(double x, double y) {
+    return Math.abs(this.x - x) < hoverSlack
+        && Math.abs(this.y - y) < hoverSlack;
+  }
 
-	public String toString() {
-		return String.format("(corner %.2f:%.2f)", x, y);
-	}
+  public boolean setHover(boolean hover) {
+    boolean changed = this.hover != hover;
+    this.hover = hover;
+    return changed;
+  }
 
-	public boolean isHoverCoordinates(double x, double y) {
-		return Math.abs(this.x - x) < hoverSlack
-				&& Math.abs(this.y - y) < hoverSlack;
-	}
+  public boolean isHover() {
+    return hover;
+  }
 
-	public boolean setHover(boolean hover) {
-		boolean changed = this.hover != hover;
-		this.hover = hover;
-		return changed;
-	}
+  public boolean isSelected() {
+    return selected;
+  }
 
-	public boolean isHover() {
-		return hover;
-	}
+  public void setSelected(boolean selected) {
+    this.selected = selected;
+  }
 
-	public boolean isSelected() {
-		return selected;
-	}
+  public GLEditorState getSelectionState(GLEditor editor) {
+    return new SelectCornerState(editor, this);
+  }
 
-	public void setSelected(boolean selected) {
-		this.selected = selected;
-	}
+  Double posAngle(double angle) {
+    return angle < 0d ? Math.PI * 2 + angle : angle;
+  }
 
-	public GLEditorState getSelectionState(GLEditor editor) {
-		return new SelectCornerState(editor, this);
-	}
+  public Double angleStartOf(Wall wall) {
+    return posAngle(Math.atan2(wall.getEndingCorner().getY() - y, wall
+        .getEndingCorner().getX()
+        - x));
+  }
 
-	Double posAngle(double angle) {
-		return angle < 0d ? Math.PI * 2 + angle : angle;
-	}
+  public Double angleEndOf(Wall wall) {
+    return posAngle(Math.atan2(wall.getStartingCorner().getY() - y, wall
+        .getStartingCorner().getX()
+        - x));
+  }
 
-	public Double angleStartOf(Wall wall) {
-		return posAngle(Math.atan2(wall.getEndingCorner().getY() - y, wall
-				.getEndingCorner().getX()
-				- x));
-	}
+  public TreeMap<Double, Wall> getWalls() {
+    TreeMap<Double, Wall> walls = new TreeMap<Double, Wall>();
+    for (Wall wall : startOf) {
+      walls.put(angleStartOf(wall), wall);
+    }
+    for (Wall wall : endOf) {
+      walls.put(angleEndOf(wall), wall);
+    }
+    return walls;
+  }
 
-	public Double angleEndOf(Wall wall) {
-		return posAngle(Math.atan2(wall.getStartingCorner().getY() - y, wall
-				.getStartingCorner().getX()
-				- x));
-	}
+  public double[] getStartLimitsOf(Wall wall) {
+    Double angle = angleStartOf(wall);
+    return getLimitsOf(wall, angle);
+  }
 
-	public TreeMap<Double, Wall> getWalls() {
-		TreeMap<Double, Wall> walls = new TreeMap<Double, Wall>();
-		for (Wall wall : startOf) {
-			walls.put(angleStartOf(wall), wall);
-		}
-		for (Wall wall : endOf) {
-			walls.put(angleEndOf(wall), wall);
-		}
-		return walls;
-	}
+  public double[] getEndLimitsOf(Wall wall) {
+    Double angle = angleEndOf(wall);
+    return getLimitsOf(wall, angle);
+  }
 
-	public double[] getStartLimitsOf(Wall wall) {
-		Double angle = angleStartOf(wall);
-		return getLimitsOf(wall, angle);
-	}
+  public double[] getLimitsOf(Wall wall, Double angle) {
+    TreeMap<Double, Wall> walls = getWalls();
+    double[] points = new double[4];
+    Double oldAngle;
+    if (angle.equals(walls.firstKey())) {
+      oldAngle = walls.lastKey();
+    } else {
+      oldAngle = walls.headMap(angle).lastKey();
+    }
+    double avg;
+    double len;
+    if (oldAngle.equals(angle)) {
+      avg = angle - Math.PI / 2;
+      len = thickness;
+    } else {
+      avg = (angle + oldAngle) / 2;
+      len = thickness / Math.sin(angle - avg);
+    }
+    points[0] = x + Math.cos(avg) * len;
+    points[1] = y + Math.sin(avg) * len;
+    if (angle.equals(walls.lastKey())) {
+      oldAngle = walls.firstKey();
+    } else {
+      SortedMap<Double, Wall> larger = walls.tailMap(angle);
+      Set<Double> angles = larger.keySet();
+      for (Double largerAngle : angles) {
+        if (largerAngle > angle) {
+          oldAngle = largerAngle;
+          break;
+        }
+      }
+    }
+    if (oldAngle.equals(angle)) {
+      avg = angle + Math.PI / 2;
+      len = thickness;
+    } else {
+      avg = (angle + oldAngle) / 2;
+      len = thickness / Math.sin(oldAngle - avg);
+    }
+    points[2] = x + Math.cos(avg) * len;
+    points[3] = y + Math.sin(avg) * len;
+    return points;
+  }
 
-	public double[] getEndLimitsOf(Wall wall) {
-		Double angle = angleEndOf(wall);
-		return getLimitsOf(wall, angle);
-	}
+  public void editorRender() {
+    double selectionRadius = 0.06d;
+    double x = getX();
+    double y = getY();
+    if (isHover()) {
+      GL.glColor3d(1d, 0d, 0d);
+    } else if (isSelected()) {
+      GL.glColor3d(0d, 0.5d, 0d);
+    } else {
+      GL.glColor3d(0d, 0d, 0d);
+    }
+    if (isSelected()) {
+      GL.glEnable(GL.GL_LINE_STIPPLE);
+      {
+        GL.glLineStipple(1, (short) 0xAAAA);
+        GL.glBegin(GL.GL_LINES);
+        {
+          GL.glVertex2d(x - selectionRadius, y - selectionRadius);
+          GL.glVertex2d(x - selectionRadius, y + selectionRadius);
+          GL.glVertex2d(x + selectionRadius, y - selectionRadius);
+          GL.glVertex2d(x + selectionRadius, y + selectionRadius);
+          GL.glVertex2d(x - selectionRadius, y - selectionRadius);
+          GL.glVertex2d(x + selectionRadius, y - selectionRadius);
+          GL.glVertex2d(x - selectionRadius, y + selectionRadius);
+          GL.glVertex2d(x + selectionRadius, y + selectionRadius);
+        }
+        GL.glEnd();
+      }
+      GL.glDisable(GL.GL_LINE_STIPPLE);
+    }
+  }
 
-	public double[] getLimitsOf(Wall wall, Double angle) {
-		TreeMap<Double, Wall> walls = getWalls();
-		double[] points = new double[4];
-		Double oldAngle = walls.lowerKey(angle);
-		if (oldAngle == null) {
-			oldAngle = walls.lastKey();
-		}
-		double avg;
-		double len;
-		if (oldAngle.equals(angle)) {
-			avg = angle - Math.PI / 2;
-			len = thickness;
-		} else {
-			avg = (angle + oldAngle) / 2;
-			len = thickness / Math.sin(angle - avg);
-		}
-		points[0] = x + Math.cos(avg) * len;
-		points[1] = y + Math.sin(avg) * len;
-		oldAngle = walls.higherKey(angle);
-		if (oldAngle == null) {
-			oldAngle = walls.firstKey();
-		}
-		if (oldAngle.equals(angle)) {
-			avg = angle + Math.PI / 2;
-			len = thickness;
-		} else {
-			avg = (angle + oldAngle) / 2;
-			len = thickness / Math.sin(oldAngle - avg);
-		}
-		points[2] = x + Math.cos(avg) * len;
-		points[3] = y + Math.sin(avg) * len;
-		return points;
-	}
-
-	public void editorRender() {
-		double selectionRadius = 0.06d;
-
-		double x = getX();
-		double y = getY();
-		if (isHover()) {
-			GL.glColor3d(1d, 0d, 0d);
-		} else if (isSelected()) {
-			GL.glColor3d(0d, 0.5d, 0d);
-		} else {
-			GL.glColor3d(0d, 0d, 0d);
-		}
-
-		if (isSelected()) {
-			GL.glEnable(GL.GL_LINE_STIPPLE);
-			{
-				GL.glLineStipple(1, (short) 0xAAAA);
-				GL.glBegin(GL.GL_LINES);
-				{
-					GL.glVertex2d(x - selectionRadius, y - selectionRadius);
-					GL.glVertex2d(x - selectionRadius, y + selectionRadius);
-					GL.glVertex2d(x + selectionRadius, y - selectionRadius);
-					GL.glVertex2d(x + selectionRadius, y + selectionRadius);
-					GL.glVertex2d(x - selectionRadius, y - selectionRadius);
-					GL.glVertex2d(x + selectionRadius, y - selectionRadius);
-					GL.glVertex2d(x - selectionRadius, y + selectionRadius);
-					GL.glVertex2d(x + selectionRadius, y + selectionRadius);
-				}
-				GL.glEnd();
-			}
-			GL.glDisable(GL.GL_LINE_STIPPLE);
-		}
-	}
-
-	public void realRender() {
-		GL.glColor3d(0d, 0d, 0d);
-		GL.glBegin(GL.GL_LINES);
-		{
-			GL.glVertex3d(x, y, 0);
-			GL.glVertex3d(x, y, Wall.height);
-		}
-		GL.glEnd();
-	}
+  public void realRender() {
+    GL.glColor3d(0d, 0d, 0d);
+    GL.glBegin(GL.GL_LINES);
+    {
+      GL.glVertex3d(x, y, 0);
+      GL.glVertex3d(x, y, Wall.height);
+    }
+    GL.glEnd();
+  }
 }
--- trunk/org.opencad.core/src/org/opencad/modelling/walls/Wall.java
+++ trunk/org.opencad.core/src/org/opencad/modelling/walls/Wall.java
@@ -6,111 +6,111 @@ import org.opencad.modelling.PrimitiveTypeRegister;
 import org.opencad.modelling.corners.Corner;
 
 public class Wall extends Primitive {
+  private static final long serialVersionUID = -8662848078904155699L;
+  static {
+    PrimitiveTypeRegister.registerPrimitiveType(Wall.class);
+  }
 
-	private static final long serialVersionUID = -8662848078904155699L;
+  Corner startingCorner, endingCorner;
 
-	static {
-		PrimitiveTypeRegister.registerPrimitiveType(Wall.class);
-	}
+  public final Corner getEndingCorner() {
+    return endingCorner;
+  }
 
-	Corner startingCorner, endingCorner;
+  public final void setEndingCorner(Corner endingCorner) {
+    if (this.endingCorner != null) {
+      this.endingCorner.removeEnd(this);
+    }
+    this.endingCorner = endingCorner;
+    if (this.endingCorner != null) {
+      this.endingCorner.addEnd(this);
+    }
+  }
 
-	public final Corner getEndingCorner() {
-		return endingCorner;
-	}
+  public final Corner getStartingCorner() {
+    return startingCorner;
+  }
 
-	public final void setEndingCorner(Corner endingCorner) {
-		if (this.endingCorner != null) {
-			this.endingCorner.removeEnd(this);
-		}
-		this.endingCorner = endingCorner;
-		if (this.endingCorner != null) {
-			this.endingCorner.addEnd(this);
-		}
-	}
+  public final void setStartingCorner(Corner startingCorner) {
+    if (this.startingCorner != null) {
+      this.startingCorner.removeStart(this);
+    }
+    this.startingCorner = startingCorner;
+    if (this.startingCorner != null) {
+      this.startingCorner.addStart(this);
+    }
+  }
 
-	public final Corner getStartingCorner() {
-		return startingCorner;
-	}
+  public Wall(Corner startingCorner, Corner endingCorner) {
+    this.startingCorner = startingCorner;
+    this.endingCorner = endingCorner;
+  }
 
-	public final void setStartingCorner(Corner startingCorner) {
-		if (this.startingCorner != null) {
-			this.startingCorner.removeStart(this);
-		}
-		this.startingCorner = startingCorner;
-		if (this.startingCorner != null) {
-			this.startingCorner.addStart(this);
-		}
-	}
+  public String toString() {
+    return String.format("(wall %s %s)", startingCorner, endingCorner);
+  }
 
-	public Wall(Corner startingCorner, Corner endingCorner) {
-		this.startingCorner = startingCorner;
-		this.endingCorner = endingCorner;
-	}
+  public double sqr(double a) {
+    return a * a;
+  }
 
-	public String toString() {
-		return String.format("(wall %s %s)", startingCorner, endingCorner);
-	}
+  public void editorRender() {
+    if (getStartingCorner() != null && getEndingCorner() != null) {
+      GL.glColor3d(0d, 0d, 0d);
+      GL.glBegin(GL.GL_LINES);
+      {
+        double[] start_points = getStartingCorner().getStartLimitsOf(this);
+        double[] end_points = getEndingCorner().getEndLimitsOf(this);
+        double[] these_points = new double[] { getStartingCorner().getX(),
+            getStartingCorner().getY(), getEndingCorner().getX(),
+            getEndingCorner().getY() };
+        int angle = (int) (Math.signum(these_points[1] - these_points[3]) + 3 * Math
+            .signum(these_points[0] - these_points[2]));
+        int angle_1 = (int) (Math.signum(start_points[1] - end_points[3]) + 3 * Math
+            .signum(start_points[0] - end_points[2]));
+        if (angle == angle_1) {
+          GL.glVertex2d(start_points[0], start_points[1]);
+          GL.glVertex2d(end_points[2], end_points[3]);
+        }
+        int angle_2 = (int) (Math.signum(start_points[3] - end_points[1]) + 3 * Math
+            .signum(start_points[2] - end_points[0]));
+        if (angle == angle_2) {
+          GL.glVertex2d(start_points[2], start_points[3]);
+          GL.glVertex2d(end_points[0], end_points[1]);
+        }
+      }
+      GL.glEnd();
+      GL.glEnable(GL.GL_LINE_STIPPLE);
+      {
+        GL.glLineStipple(1, (short) 0x43f0);
+        GL.glBegin(GL.GL_LINES);
+        {
+          GL.glVertex2d(getStartingCorner().getX(), getStartingCorner().getY());
+          GL.glVertex2d(getEndingCorner().getX(), getEndingCorner().getY());
+        }
+        GL.glEnd();
+      }
+      GL.glDisable(GL.GL_LINE_STIPPLE);
+    }
+  }
 
-	public void editorRender() {
-		if (getStartingCorner() != null && getEndingCorner() != null) {
-			GL.glColor3d(0d, 0d, 0d);
-			GL.glBegin(GL.GL_LINES);
-			{
-				double[] start_points = getStartingCorner().getStartLimitsOf(
-						this);
-				double[] end_points = getEndingCorner().getEndLimitsOf(this);
-				double angle = Math.atan2(getStartingCorner().getY()
-						- getEndingCorner().getY(), getStartingCorner().getX()
-						- getEndingCorner().getX());
-				double angle_1 = Math.atan2(start_points[1] - end_points[3],
-						start_points[0] - end_points[2]);
-				double e = 1e-15;
-				if (Math.abs(angle - angle_1) < e) {
-					GL.glVertex2d(start_points[0], start_points[1]);
-					GL.glVertex2d(end_points[2], end_points[3]);
-				}
-				double angle_2 = Math.atan2(start_points[3] - end_points[1],
-						start_points[2] - end_points[0]);
-				if (Math.abs(angle - angle_2) < e) {
-					GL.glVertex2d(start_points[2], start_points[3]);
-					GL.glVertex2d(end_points[0], end_points[1]);
-				}
-			}
-			GL.glEnd();
-			GL.glEnable(GL.GL_LINE_STIPPLE);
-			{
-				GL.glLineStipple(1, (short) 0xAAAA);
-				GL.glBegin(GL.GL_LINES);
-				{
-					GL.glVertex2d(getStartingCorner().getX(),
-							getStartingCorner().getY());
-					GL.glVertex2d(getEndingCorner().getX(), getEndingCorner()
-							.getY());
-				}
-				GL.glEnd();
-			}
-			GL.glDisable(GL.GL_LINE_STIPPLE);
-		}
-	}
+  public static final double height = 3.5d;
 
-	public static final double height = 3.5d;
-
-	public void realRender() {
-		if (getStartingCorner() != null && getEndingCorner() != null) {
-			GL.glColor3d(0d, 0d, 0d);
-			GL.glBegin(GL.GL_LINES);
-			{
-				GL.glVertex3d(getStartingCorner().getX(), getStartingCorner()
-						.getY(), 0);
-				GL.glVertex3d(getEndingCorner().getX(), getEndingCorner()
-						.getY(), 0);
-				GL.glVertex3d(getStartingCorner().getX(), getStartingCorner()
-						.getY(), height);
-				GL.glVertex3d(getEndingCorner().getX(), getEndingCorner()
-						.getY(), height);
-			}
-			GL.glEnd();
-		}
-	}
+  public void realRender() {
+    if (getStartingCorner() != null && getEndingCorner() != null) {
+      GL.glColor3d(0d, 0d, 0d);
+      GL.glBegin(GL.GL_LINES);
+      {
+        GL
+            .glVertex3d(getStartingCorner().getX(), getStartingCorner().getY(),
+                0);
+        GL.glVertex3d(getEndingCorner().getX(), getEndingCorner().getY(), 0);
+        GL.glVertex3d(getStartingCorner().getX(), getStartingCorner().getY(),
+            height);
+        GL.glVertex3d(getEndingCorner().getX(), getEndingCorner().getY(),
+            height);
+      }
+      GL.glEnd();
+    }
+  }
 }
